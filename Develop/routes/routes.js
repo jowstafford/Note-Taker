3984c11a880d12404d276d669590f29865e20239
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-30 12:31:17 -0500

    created GET *  HTML route to return index.html in routes.js

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -2,4 +2,7 @@ module.exports = (routes) => {
   routes.get("/notes", function (res) {
     res.sendFile(path.join("../public/notes.html"));
   });
+  routes.get("*", function (res) {
+    res.sendFile(path.join("../public/index.html"));
+  });
 };
