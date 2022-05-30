1b623ec60ae9d14d067c56d52d0da770f011489c
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-30 12:56:26 -0500

    POST /api/notes recieves a new note to save on the request body, added to the db.json file, and new note returned to the client

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -8,10 +8,21 @@ module.exports = (routes) => {
     res.sendFile(path.join("../public/index.html"));
   });
 
-  fs.readFile("db/db.json", (userData) => {
+  fs.readFile("/db/db.json", (userData) => {
     const noteTaker = JSON.parse(userData);
     routes.get("/api/notes", function (res) {
       res.json(noteTaker);
     });
+  });
+
+  function Write() {
+    fs.writeFile("/db/db.json", JSON.stringify(notes, "\t"), () => {
+      return true;
+    });
+  }
+  routes.post("/api/notes", function (res) {
+    const Note = res.body;
+    noteTaker.push(Note);
+    Write();
   });
 };
