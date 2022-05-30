b5ce126b72ad6ca666968466e8a8e2df1fe14db0
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-05-30 12:42:57 -0500

    GET /api/notes reads the db.json file and returns all saved notes as JSON

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -1,8 +1,17 @@
+const fs = require("fs");
+
 module.exports = (routes) => {
   routes.get("/notes", function (res) {
     res.sendFile(path.join("../public/notes.html"));
   });
   routes.get("*", function (res) {
     res.sendFile(path.join("../public/index.html"));
+  });
+
+  fs.readFile("db/db.json", (userData) => {
+    const noteTaker = JSON.parse(userData);
+    routes.get("/api/notes", function (res) {
+      res.json(noteTaker);
+    });
   });
 };
